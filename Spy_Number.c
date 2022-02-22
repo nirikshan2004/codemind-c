12ed8886288271b34fbb2167e9e3c9be3373bfa2
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-02-22 14:28:38 +0530

    #include<stdio.h> int main() {     int num,i;     scanf("%d",&num);     int sum=0,mult=1,rem;     while(num!=0)     {         rem=num%10;         sum+=rem;         mult*=rem;         num/=10;     }
       if(sum==mult)
        printf("Spy Number");
       else
        printf("Not Spy Number");
    }

---
+++ Spy_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    scanf("%d",&num);
+    int sum=0,mult=1,rem;
+    while(num!=0)
+    {
+        rem=num%10;
+        sum+=rem;
+        mult*=rem;
+        num/=10;
+    }
+
+   if(sum==mult)
+    printf("Spy Number");
+   else
+    printf("Not Spy Number");
+}
