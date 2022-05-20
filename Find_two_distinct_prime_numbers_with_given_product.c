3e35020607ea1e732d8a7e385405b2c66d2c9be7
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-05-20 15:54:41 +0530

    #include<stdio.h> int main() {     int n,i,j,k,p,q,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=2;j<n;j++)         {             for(k=2;k<n;k++)             {                 if(i%j!=0 && i%k!=0)                 {                     if(j*k==n)                     {                         p=j;                         q=k;                         c++;                         break;                     }                 }             }         }     }     if(c==0)     {         printf("-1");     }     else     {         printf("%d %d",q,p);     } }

---
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k,p,q,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=2;j<n;j++)
+        {
+            for(k=2;k<n;k++)
+            {
+                if(i%j!=0 && i%k!=0)
+                {
+                    if(j*k==n)
+                    {
+                        p=j;
+                        q=k;
+                        c++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d %d",q,p);
+    }
+}
