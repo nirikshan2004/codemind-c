e414b52423afe3a387acfabc439aae7c2460dbe5
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-09-11 15:54:09 +0530

    #include<stdio.h> int main() {     int n,arr[100],a,b,i,c=0,min=1000;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if((arr[i]<a&&arr[i]<b)||(arr[i]>a&&arr[i]>b))         {            if(min>arr[i])            {                min=arr[i];                c++;            }         }     }     if(c!=0)     {         printf("%d",min);     }     else     {     printf("-1");     } }

---
+++ minimum_not_between_A_and_B.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],a,b,i,c=0,min=1000;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if((arr[i]<a&&arr[i]<b)||(arr[i]>a&&arr[i]>b))
+        {
+           if(min>arr[i])
+           {
+               min=arr[i];
+               c++;
+           }
+        }
+    }
+    if(c!=0)
+    {
+        printf("%d",min);
+    }
+    else
+    {
+    printf("-1");
+    }
+}
