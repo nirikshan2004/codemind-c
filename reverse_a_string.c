0dca0d16655d8776306c7dd176d5655c2f48ea20
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-09-11 12:59:59 +0530

    #include<stdio.h> int stringlen(char *str) {     int i;     for(i=0;str[i]!=NULL;i++)     {
        }
        return i;
    }
    void stringreverse(char *str)
    {
        char temp;
        int i,j,len;
        len=stringlen(str);
        i=0;
        j=len-1;
        while(i<j)
        {
            temp=str[i];
            str[i]=str[j];
            str[j]=temp;
            i++;
            j--;
        }
    }
    int main()
    {    char str[100];
        scanf("%[^
    ]s",str);
        stringreverse(str);
        printf("%s",str);
    }

---
+++ reverse_a_string.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int stringlen(char *str)
+{
+    int i;
+    for(i=0;str[i]!=NULL;i++)
+    {
+        
+    }
+    return i;
+}
+void stringreverse(char *str)
+{
+    char temp;
+    int i,j,len;
+    len=stringlen(str);
+    i=0;
+    j=len-1;
+    while(i<j)
+    {
+        temp=str[i];
+        str[i]=str[j];
+        str[j]=temp;
+        i++;
+        j--;
+    }
+}
+int main()
+{    char str[100];
+    scanf("%[^
+]s",str);
+    stringreverse(str);
+    printf("%s",str);
+}
