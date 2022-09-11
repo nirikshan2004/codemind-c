e832e0cfa2494402dfd9bf6b95b9555b90bf8488
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-09-11 13:30:23 +0530

    #include<stdio.h> int main() {     char str[100];     int i,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>=65 &&str[i]<=90)         {             c++;         }     }     printf("%d",c); }

---
+++ uppercase_letters_in_a_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,c=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>=65 &&str[i]<=90)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
