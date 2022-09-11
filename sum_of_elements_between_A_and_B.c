45116733576d59857a5b127c902b98429426ad84
nirikshan2004 <97437022+nirikshan2004@users.noreply.github.com>
2022-09-11 15:52:04 +0530

    #include<stdio.h> int main() {     int n,arr[100],a,b,i,c=0,min=1000;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if((arr[i]<=a&&arr[i]>=b)||(arr[i]>=a&&arr[i]<=b))         {            c=c+arr[i];         }     }     printf("%d",c); }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],a,b,i,c=0,min=1000;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if((arr[i]<=a&&arr[i]>=b)||(arr[i]>=a&&arr[i]<=b))
+        {
+           c=c+arr[i];
+        }
+    }
+    printf("%d",c);
+}
